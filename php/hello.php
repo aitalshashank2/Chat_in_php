aca9c9b0f611bf4a9c29c40f66700f333f4955ca
Shashank Aital <aitalshashank2@gmail.com>
2020-03-23 15:45:15 +0530

    Login Implemented

---
+++ php/hello.php
@@ -0,0 +1,46 @@
+<?php
+    include 'connect.php';
+
+    session_start();
+    $sessID = $_SESSION['userSessHash'];
+
+    $q = "SELECT * FROM shashank_Sessions WHERE Session = '$sessID';";
+    $res = $conn->query($q);
+
+    //Cookie generation
+    if($_SESSION['RememberStatus'] == 1){
+        $h = RandHashGen();
+        $i = 0;
+        while($row = mysqli_fetch_assoc($res)){
+            $usrName[$i] = $row['UserName'];
+            $i = $i + 1;
+        }
+        $user = $usrName[0];
+        $t = time() + 2*86400;
+
+        $q = "DELETE FROM shashank_Preserve WHERE UserName='$user'";
+        $conn->query($q);
+
+        $q = "INSERT INTO shashank_Preserve (RetUserHash, UserName, ExpTime) VALUES ('$h', '$user', $t);";
+        $conn->query($q);
+
+        setcookie('CIPPreserve', $h);
+
+        $_SESSION['RememberStatus'] = 0;
+    }
+
+    if(mysqli_num_rows($res) >= 1){
+        echo "Hello";
+    }else{
+
+        $prior = $_COOKIE['CIPPreserve'];
+        $q = "SELECT * FROM shashank_Preserve WHERE RetUserHash='$prior';";
+        $res = $conn->query($q);
+        if(mysqli_num_rows($res) >= 1){
+            echo "Hello";
+
+        }else{        
+            die(header("Location: ../html/signIn.html"));
+        }
+    }
+?>
